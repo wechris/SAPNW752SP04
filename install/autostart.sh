5d948e8c57bc145bae651d3d93d25b0fa34a951b
wechris <weiss_christian@web.de>
2019-11-09 18:16:13 +0100

    enable installation and autostart

---
+++ install/autostart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Enable autostart..."
+sudo echo "Autostart = 1" >> /usr/sap/NPL/SYS/profile/DEFAULT.PFL
--- install/sapinst.sh
+++ install/sapinst.sh
@@ -8,8 +8,19 @@ sudo sed -i.bak '/127.*vhcalnplci/d' /etc/hosts
 echo "install tcsh..."
 zypper --non-interactive --gpg-auto-import-keys in --auto-agree-with-licenses tcsh
 
-echo "chmod install.sh..."
-if [ -f /vagrant/sapinst/install.sh ]
-then
-  sudo chmod +x /vagrant/sapinst/install.sh
+echo "Check if SAP NW is installed..."
+if [ -e /usr/sap ]; then
+    echo "  seems SAP is already installed."
+    exit 0
 fi
+
+echo "chmod install.sh..."
+if [ ! -f /vagrant/sapinst/install.sh ]; then
+    echo "  install.sh NOT found, NW installation not found, continue manually"
+    exit 0
+fi
+
+echo "Try to install SAP NW..."
+sudo cd /vagrant/sapinst/
+sudo expect -f /vagrant/install/install_nw.expect
+
